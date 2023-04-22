bc0ec2fc2f296f61b36787d23e4fd3f5b82bd98f
Flor Palma <whatsgoingonlion@gmail.com>
2023-04-22 01:05:36 -0600

    make it better

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+const expressLayots = require('express-ejs-layouts')
+
+const PORT = process.env.PORT || 3000;
+
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+
+app.use(expressLayots)
+
+app.get('/',(req,res)=>{
+    res.render('inicio')
+})
+
+app.get('/contacto',(req,res)=>{
+    res.render('contacto')
+})
+
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
