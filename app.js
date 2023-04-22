a65d234a44cea5ef457b238540fd40747cc05cfb
Flor Palma <whatsgoingonlion@gmail.com>
2023-04-22 01:32:58 -0600

    make it better

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 const expressLayots = require('express-ejs-layouts')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 
 
 app.set('view engine','ejs')
